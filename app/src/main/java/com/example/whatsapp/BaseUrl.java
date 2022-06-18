2ac46a554883a342baf1f8333de4c6f4847167de
Uri Greitser <urigreitser@gmail.com>
2022-06-18 21:18:25 +0300

    merge

---
+++ app/src/main/java/com/example/whatsapp/BaseUrl.java
@@ -0,0 +1,11 @@
+package com.example.whatsapp;
+
+public class BaseUrl {
+    public static  String BASE_URL = "https://10.0.2.2:7290/";
+
+    public BaseUrl() {
+    }
+    public static void setBaseUrl(String baseurl) {
+        BASE_URL = baseurl;
+    }
+}
